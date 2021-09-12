fb6406f82e4ea5cd5a5c3e670d453a9a4dfc6c6f
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-12 13:56:03 +0000

    update firestore.tsに関数の追加
    firestore.tsのイメージ概略の作成。確認してもらった後に細かい内容の記述

---
+++ src/firebase/firestore.ts
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+const getApplications = () => {
+  const appId = ""; // アプリIDを呼び出す操作
+  const appIcon = ""; // アプリのアイコンを呼び出す操作
+  // 以下に他にも呼び出すものを記述
+};
+
+const fetchApplication = () => {};
+
+const fetchUser = () => {};
