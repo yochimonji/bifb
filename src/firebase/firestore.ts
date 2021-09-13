4310448ffacfdccad743e0410fa971eb15db6059
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-13 08:19:36 +0000

    add product collectionへの値の追加機能の実装

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,5 +1,28 @@
-import firebase from "firebase/compat/app";
+import firebase from "firebase/compat";
 import "firebase/compat/firestore";
+import firebaseConfig from "./config";
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// 新しいドキュメントの作成
+// Add a new document "test"(ex) in collection "product"
+const productData = {
+  productId: Number,
+  icon: null,
+  title: String,
+  serId: Number,
+  postData: Date,
+  editData: Date,
+  feedback: Object,
+};
+const resProduct = await db.collection("product").doc("test").set(productData);
+
+// 上と同様にして、
+// feedback collection
+// user collection
+// tag collection
+// の追加についても必要
 
 const getApplications = () => {
   const appId = ""; // アプリIDを呼び出す操作
