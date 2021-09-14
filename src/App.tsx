889add292e153fde3e6a110291c5f1e2c8dc2465
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-14 01:32:57 +0000

    add psotProduct関数の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-
 import { SampleBox1, SampleBox2 } from "./components/index";
+
+// 動作確認用のファイルのimport及び実行結果の表示
+import { postProduct } from "./firebase/firestore";
+
+const tmp = postProduct("a", "b", "c", "d", "e", ["f", "g"], "h", "i");
+const result = tmp.toString();
 
 const App = (): JSX.Element => (
   <ChakraProvider theme={theme}>
     <SampleBox1 text="開発がんばろー" />
     <SampleBox2 text="おー！！" num={88888888} />
+    <SampleBox1 text={result} />
   </ChakraProvider>
 );
 
--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,34 +1,57 @@
-import firebase from "firebase/compat";
+import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import firebaseConfig from "./config";
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 // 新しいドキュメントの作成
-// Add a new document "test"(ex) in collection "product"
-const productData = {
-  productId: Number,
-  icon: null,
-  title: String,
-  serId: Number,
-  postData: Date,
-  editData: Date,
-  feedback: Object,
+// product collectionに作品を登録
+export const postProduct = async (
+  productTitle: string,
+  productAbstract: string,
+  productIconUrl: string,
+  githubUrl: string,
+  productUrl: string,
+  tags: string[],
+  mainText: string,
+  userUid: string
+): Promise<boolean> => {
+  const docProduct = await addDoc(collection(db, "product"), {
+    productTitle,
+    productAbstract,
+    productIconUrl,
+    githubUrl,
+    productUrl,
+    tags,
+    mainText,
+    postData: new Date().toLocaleString(),
+    editData: new Date().toLocaleString(),
+    userUid,
+  });
+  if (!docProduct.id) {
+    return false;
+  }
+  // eslint-disable-next-line no-console
+  console.log("document written with ID", docProduct.id);
+  return true;
 };
-const resProduct = await db.collection("product").doc("test").set(productData);
 
-// 上と同様にして、
-// feedback collection
-// user collection
-// tag collection
-// の追加についても必要
-
-const getProducts = async (tag: string) => {
-  const productList = await db.collection("product").doc(tag).get();
-  return productList;
+// 作品を追加するときのタグが存在していなかったら、tag collectionにタグを追加
+export const postTags = async (tags: string[]): Promise<boolean> => {
+  const docProduct = await addDoc(collection(db, "tags"), {});
+  if (!docProduct.id) {
+    return false;
+  }
+  // eslint-disable-next-line no-console
+  console.log("document written with ID", docProduct.id);
+  return true;
 };
 
-const fetchProduct = () => {};
+// 以下データの取得に関する記述
+// export const fetchProducts = () => {};
 
-const fetchUser = () => {};
+// const fetchProduct = () => {};
+
+// const fetchUser = () => {};
