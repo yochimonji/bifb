0d62337e8ee8ecb9dbb693060c7c4f9fc9286478
yochimonji <yochimonji@gmail.com>
2021-09-15 02:54:19 +0000

    rm 不要なファイル・関数の削除
    サンプルで追加していたファイルと関数を削除した

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 
-import { SampleBox1, SampleBox2 } from "./components/index";
+import {} from "./components/index";
 
-const App = (): JSX.Element => (
-  <ChakraProvider theme={theme}>
-    <SampleBox1 text="開発がんばろー" />
-    <SampleBox2 text="おー！！" num={88888888} />
-  </ChakraProvider>
-);
+const App = (): JSX.Element => <ChakraProvider theme={theme} />;
 
 export default App;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,1 @@
-// エントリーポイント
-// コンポーネントと型定義の書き方の例
-export { default as SampleBox1 } from "./SampleBox1";
-export { default as SampleBox2 } from "./SampleBox2";
-// 複数のコンポーネントで同じ型定義を使う場合別ファイルに分けた方が便利
-export type { SampleBox2Props } from "./SampleBox2Props";
+
