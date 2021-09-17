de152b2d493fb7278583389952f3701c1bbc17d4
yochimonji <yochimonji@gmail.com>
2021-09-17 17:25:50 +0000

    add タイトルと概要の入力欄を作成

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,6 +1,66 @@
-import React from "react";
-import { Heading } from "@chakra-ui/react";
+import React, { ReactEventHandler, useState } from "react";
+import {
+  Input,
+  Textarea,
+  HStack,
+  Stack,
+  Image,
+  Box,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { BsImage } from "react-icons/bs";
 
-const Post = (): JSX.Element => <Heading size="4xl">POST</Heading>;
+const Post = (): JSX.Element => {
+  const [title, setTitle] = useState("");
+  const [abstract, setAbstract] = useState("");
+  const [iconUrl, setIconUrl] = useState("");
+  const [githubUrl, setGithubUrl] = useState("");
+  const [productUrl, setProductUrl] = useState("");
+  const [tags, setTags] = useState("");
+  const [mainText, setMainText] = useState("");
+
+  const handleTitle: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleAbstract: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setAbstract(event.target.value);
+  };
+
+  return (
+    <Stack>
+      <HStack spacing="4" pt="8" align="start">
+        <Stack>
+          <Image minW="200px" h="200px" border="gray" />
+          <Button leftIcon={<BsImage />} variant="ghost">
+            変更する
+          </Button>
+        </Stack>
+        <Stack w="100%" h="200px">
+          <Input
+            w="100%"
+            h="40%"
+            placeholder="作品タイトル"
+            fontSize="xl"
+            variant="flushed"
+            value={title}
+            onChange={handleTitle}
+          />
+          <Input
+            w="100%"
+            h="60%"
+            placeholder="この作品を一言で表すと？"
+            variant="flushed"
+            value={abstract}
+            onChange={handleAbstract}
+          />
+        </Stack>
+      </HStack>
+    </Stack>
+  );
+};
 
 export default Post;
