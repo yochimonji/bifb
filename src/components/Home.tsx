e3ee62ca15c0d1f271aa12889d6c25a346f0b91d
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-18 17:35:12 +0000

    update Homeページベタ書き概略完成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,7 +11,96 @@ import {
   Image,
   Avatar,
   Text,
+  IconButton,
 } from "@chakra-ui/react";
+import { PlusSquareIcon } from "@chakra-ui/icons";
+
+function Product() {
+  return (
+    <Box
+      height="210px"
+      shadow="md"
+      boederWidth="1px"
+      flex="1"
+      borderRadius="md"
+    >
+      <HStack spacing={0}>
+        <Box w="30%" h="210">
+          <VStack spacing={0}>
+            <Box w="100%" h="150px">
+              <Image
+                src="https://bit.ly/sage-adebayo"
+                boxsize="100px"
+                padding="10px"
+                alignItems="center"
+              />
+            </Box>
+            <Box w="100%" h="60px">
+              <HStack spacing={0}>
+                <Box w="30%" h="60px" padding="10px" margin="auto">
+                  <Avatar src="https://bit.ly/broken-link" size="sm" />
+                </Box>
+                <Box
+                  w="70%"
+                  h="60px"
+                  fontSize="xs"
+                  textAlign="center"
+                  padding="12.5px"
+                >
+                  <Text fontSize="15px">yochimonji</Text>
+                </Box>
+              </HStack>
+            </Box>
+          </VStack>
+        </Box>
+        <Box w="70%" h="210px">
+          <Box w="100%" h="70px" textAlign="left" padding="12.5px">
+            <Text fontSize="2xl">ラクスケ</Text>
+          </Box>
+          <Box w="100%" h="70px" textAlign="left">
+            <Text fontSize="lg">
+              予定に合わせて自動でタスクを登録するアプリ
+            </Text>
+          </Box>
+          <Box w="100%" h="70px">
+            <HStack spacing={0}>
+              <Box w="80%" h="70px">
+                <VStack spacing={0}>
+                  <Box w="100%" h="20px" />
+                  <Box w="100%" h="25px" textAlign="center">
+                    <Text fontSize="sm">2021年08月28日公開</Text>
+                  </Box>
+                  <Box w="100%" h="25px" textAlign="center">
+                    <Text fontSize="sm">2021年09月10日更新</Text>
+                  </Box>
+                </VStack>
+              </Box>
+              <Box w="20%" h="70px">
+                <VStack spacing={0}>
+                  <Box w="100%" h="35px" />
+                  <Box w="100%" h="35px">
+                    <HStack spacing={0}>
+                      <Box w="50%" h="35px" alignItems="center">
+                        <IconButton
+                          aria-label="Search database"
+                          size="xs"
+                          icon={<PlusSquareIcon />}
+                        />
+                      </Box>
+                      <Box w="50%" h="35px" textAlign="left" padding={2}>
+                        <Text fontSize="5px">10</Text>
+                      </Box>
+                    </HStack>
+                  </Box>
+                </VStack>
+              </Box>
+            </HStack>
+          </Box>
+        </Box>
+      </HStack>
+    </Box>
+  );
+}
 
 const Home = (): JSX.Element => (
   <VStack spacing={10} align="stretch">
@@ -21,7 +110,7 @@ const Home = (): JSX.Element => (
           w="15%"
           h="100px"
           padding="35px"
-          textAlign="center"
+          textAlign="left"
           alignItems="center"
         >
           検索条件:
@@ -63,44 +152,13 @@ const Home = (): JSX.Element => (
         </Box>
       </HStack>
     </Box>
+
     <Box h="600px">
       <SimpleGrid columns={2} spacing={10}>
-        <Box bg="#90CE9C" height="210px">
-          <HStack spacing={0}>
-            <Box w="30%" h="210">
-              <VStack spacing={0}>
-                <Box w="100%" h="150px">
-                  <Image
-                    src="https://bit.ly/sage-adebayo"
-                    boxsize="100px"
-                    padding="10px"
-                    alignItems="center"
-                  />
-                </Box>
-                <Box w="100%" h="60px">
-                  <HStack spacing={0}>
-                    <Box w="30%" h="60px" padding="10px" margin="auto">
-                      <Avatar src="https://bit.ly/broken-link" size="sm" />
-                    </Box>
-                    <Box
-                      w="70%"
-                      h="60px"
-                      fontSize="xs"
-                      textAlign="center"
-                      padding="12.5px"
-                    >
-                      <Text fontSize="15px">yochimonji</Text>
-                    </Box>
-                  </HStack>
-                </Box>
-              </VStack>
-            </Box>
-            <Box w="70%" h="210px" bg="blue.100">
-          </HStack>
-        </Box>
-        <Box bg="#90CE9C" height="210px" />
-        <Box bg="#90CE9C" height="210px" />
-        <Box bg="#90CE9C" height="210px" />
+        {Product()}
+        {Product()}
+        {Product()}
+        {Product()}
       </SimpleGrid>
     </Box>
   </VStack>
