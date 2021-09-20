a8f87f154139a69288241d961449fdd1404be9b4
yochimonji <yochimonji@gmail.com>
2021-09-20 19:37:51 +0000

    update asyncで書き直した

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -62,28 +62,32 @@ export const AuthProvider = (props: AuthProviderProps): JSX.Element => {
     }
   };
 
+  //
+  const register = async () => {
+    if (currentUser != null && currentUser)
+      await postUserInfo(
+        currentUser?.displayName as string,
+        currentUser?.photoURL as string,
+        "よろしくお願いします。",
+        "",
+        "",
+        "",
+        [""],
+        [""],
+        currentUser?.uid
+      );
+  };
+
   /**
    * Firestoreにユーザーが登録してあるかをチェックして登録してない場合に登録する関数
    * @param user Firestoreから取得したユーザー情報
    */
-  const isRegistered = (user: User | null) => {
+  const isRegistered = async (user: User | null): Promise<void> => {
     if (user) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const tmpUserInfo = fetchUserInfo(user.uid).then((userInfo) => {
-        if (!userInfo) {
-          postUserInfo(
-            currentUser?.displayName as string,
-            currentUser?.photoURL as string,
-            "よろしくお願いします。",
-            "",
-            "",
-            "",
-            [""],
-            [""],
-            currentUser?.uid as string
-          ).catch(() => {});
-        }
-      });
+      const userInfo = await fetchUserInfo(user.uid);
+      if (!userInfo) {
+        await register();
+      }
     }
   };
 
@@ -92,10 +96,11 @@ export const AuthProvider = (props: AuthProviderProps): JSX.Element => {
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
       isRegistered(user);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [auth]);
+  }, [auth, currentUser]);
 
   return (
     // Contextを使用して認証に必要な情報をコンポーネントツリーに流し込む。
