cdfe65d6cd1e108b0608d540c663820732978d24
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-21 09:51:58 +0000

    update いいね数の部分の変数化

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { HStack, Icon, Text, Box } from "@chakra-ui/react";
 import { AiFillHeart } from "react-icons/ai";
 
-const Like = (): JSX.Element => (
+type SumLikeProps = {
+  sumLike: number;
+};
+
+const Like = (props: SumLikeProps): JSX.Element => (
   <HStack alignItems="center" spacing={0}>
     <Box w="50%" h="50%" alignItems="center">
       <Icon aria-label="center" color="#EEB6B7" as={AiFillHeart} />
     </Box>
     <Text w="50%" h="100%" textAlign="center" fontSize="md">
-      10
+      {props.sumLike}
     </Text>
   </HStack>
 );
