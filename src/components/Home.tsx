b77bf4e31149b99339b3293709dc64b84ef1d126
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-09-21 02:33:56 +0000

    add Selet選択の状態を取得

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   HStack,
   VStack,
@@ -14,60 +14,66 @@ import { DisplayProduct } from "./index";
 // 作品情報の表示
 // 実際には、作品情報を変数として、テンプレート枠にその情報を埋め込んでいく
 
-const Home = (): JSX.Element => (
-  <VStack spacing={10} align="stretch">
-    {/* 上段(検索条件・トレンド等の選択) */}
-    <HStack w="100%" spacing="0px" alignItems="center" flexWrap="wrap">
-      <Box w="10%" padding="37px 20px 35px 0px" minW="90px">
-        検索条件:
-      </Box>
-      <HStack w="70%" textAlign="center" spacing={4} minW="450px">
-        {["React", "Typescript", "JavaScript", "C++", "Webアプリ"].map(
-          (tag) => (
-            <Tag
-              size="lg"
-              key="lg"
-              borderRadius="full"
-              variant="solid"
-              bg="#DEEFF1"
-              textColor="black"
-              justfy="left"
-            >
-              <TagLabel>{tag}</TagLabel>
-              <TagCloseButton />
-            </Tag>
-          )
-        )}
-      </HStack>
-      <Box w="20%" padding="30px 0px">
-        <Select>
-          <option value="TREND">トレンド</option>
-          <option value="NEW">新着</option>
-          <option value="LikeLarge">いいね数(多い順)</option>
-          <option value="LikeLarge">いいね数(少ない順)</option>
-        </Select>
-      </Box>
-    </HStack>
+const Home = (): JSX.Element => {
+  // Selectがどの状態にあるのかの把握
+  const [sortType, setSortType] = useState("");
 
-    {/* 作品一覧の表示 */}
-    <SimpleGrid
-      w="100%"
-      columns={[1, null, 2]}
-      spacingX="50px"
-      spacingY="50px"
-      justifyItems="center"
-    >
-      <DisplayProduct />
-      <DisplayProduct />
-      <DisplayProduct />
-      <DisplayProduct />
-      {/* <Box w="100%" minW="400px" maxW="450px" h="234.9px" bg="green.100" />
-      <Box w="100%" minW="400px" maxW="450px" h="234.9px" bg="green.200" />
-      <Box w="100%" minW="400px" maxW="450px" h="234.9px" bg="green.300" />
-      <Box w="100%" minW="400px" maxW="450px" h="234.9px" bg="green.400" />
-      <Box w="100%" minW="400px" maxW="450px" h="234.9px" bg="green.300" />
-      <Box w="100%" minW="400px" maxW="450px" h="234.9px" bg="green.400" /> */}
-    </SimpleGrid>
-  </VStack>
-);
+  // sortTypeの選択の変更を認識する関数
+  const onChangeSortType: React.ChangeEventHandler<HTMLSelectElement> = (
+    event
+  ) => {
+    setSortType(event.target.value);
+  };
+
+  return (
+    <VStack spacing={10} align="stretch">
+      {/* 上段(検索条件・トレンド等の選択) */}
+      <HStack w="100%" spacing="0px" alignItems="center" flexWrap="wrap">
+        <Box w="10%" padding="37px 20px 35px 0px" minW="90px">
+          検索条件:
+        </Box>
+        <HStack w="70%" textAlign="center" spacing={4} minW="450px">
+          {["React", "Typescript", "JavaScript", "C++", "Webアプリ"].map(
+            (tag) => (
+              <Tag
+                size="lg"
+                key="lg"
+                borderRadius="full"
+                variant="solid"
+                bg="#DEEFF1"
+                textColor="black"
+                justfy="left"
+              >
+                <TagLabel>{tag}</TagLabel>
+                <TagCloseButton />
+              </Tag>
+            )
+          )}
+        </HStack>
+        <Box w="20%" padding="30px 0px">
+          <Select name="sortType" onChange={onChangeSortType}>
+            <option value="TREND">トレンド</option>
+            <option value="NEW">新着</option>
+            <option value="LikeLarge">いいね数(多い順)</option>
+            <option value="LikeSmall">いいね数(少ない順)</option>
+          </Select>
+        </Box>
+      </HStack>
+
+      {/* 作品一覧の表示 */}
+      <SimpleGrid
+        w="100%"
+        columns={[1, null, 2]}
+        spacingX="50px"
+        spacingY="50px"
+        justifyItems="center"
+      >
+        <DisplayProduct />
+        <DisplayProduct />
+        <DisplayProduct />
+        <DisplayProduct />
+      </SimpleGrid>
+    </VStack>
+  );
+};
 export default Home;
