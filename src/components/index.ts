affad218ba8c58c5630bbb1b13be3bc1a320b95c
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-12-01 07:48:30 +0000

    fix: ファイル名の変更
    ファイル名に複数形を用いないように変更

--- src/components/index.ts
+++ src/components/index.ts
@@ -18,5 +18,5 @@ export { default as MarkdownPreview } from "./common_part/MarkdownPreview";
 export { default as DisplayProduct } from "./common_part/DisplayProduct";
 export { default as DisplayProducts } from "./common_part/DisplayProducts";
 export { default as Like } from "./common_part/Like";
-export { default as DisplayTags } from "./Home/DisplayTags";
+export { default as DisplayTagList } from "./Home/DisplayTagList";
 export { default as postImage } from "./common_part/postImage";
