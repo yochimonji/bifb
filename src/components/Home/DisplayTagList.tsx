52ffc0b957a3e64d8cf041c8726aee1f93f6eda6
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-12-02 21:36:17 +0900

    refactor: タグを変数で表示するように変更

--- src/components/Home/DisplayTagList.tsx
+++ src/components/Home/DisplayTagList.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import { Tag, TagLabel, TagCloseButton, HStack } from "@chakra-ui/react";
 
-const DisplayTags = (): JSX.Element => {
-  const tagList: string[] = ["React", "TypeScript"];
-  return (
-    <HStack w="70%" textAlign="center" spacing={4} minW="450px">
-      {tagList.map((tag) => (
-        <Tag
-          key={tag}
-          size="lg"
-          borderRadius="full"
-          variant="solid"
-          bg="#DEEFF1"
-          textColor="black"
-          justfy="left"
-        >
-          <TagLabel>{tag}</TagLabel>
-          <TagCloseButton />
-        </Tag>
-      ))}
-    </HStack>
-  );
+type Props = {
+  tagList: string[];
 };
+
+const DisplayTags = (props: Props): JSX.Element => (
+  <HStack w="70%" textAlign="center" spacing={4} minW="450px">
+    {props.tagList.map((tag) => (
+      <Tag
+        key={tag}
+        size="lg"
+        borderRadius="full"
+        variant="solid"
+        bg="#DEEFF1"
+        textColor="black"
+        justfy="left"
+      >
+        <TagLabel>{tag}</TagLabel>
+        <TagCloseButton />
+      </Tag>
+    ))}
+  </HStack>
+);
 
 export default DisplayTags;
