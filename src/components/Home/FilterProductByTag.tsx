bef275e3f99defb18474458ef1f05b406730da5e
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2021-12-11 11:48:57 +0900

    feat: 作品の表示

--- src/components/Home/FilterProductByTag.tsx
+++ src/components/Home/FilterProductByTag.tsx
@@ -1,5 +1,49 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { SimpleGrid } from "@chakra-ui/react";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  QuerySnapshot,
+} from "firebase/firestore";
 import React from "react";
+import DisplayProduct from "../common_part/DisplayProduct";
 
-export const FilterProductByTag = () => {};
+type Props = {
+  productData: QuerySnapshot<DocumentData> | undefined;
+};
+export const FilterProductByTag = (props: Props): JSX.Element => {
+  const tmp = "a";
+
+  return (
+    <SimpleGrid
+      w="100%"
+      columns={[1, null, 2]}
+      spacingX="50px"
+      spacingY="50px"
+      justifyItems="center"
+    >
+      {props.productData &&
+        props.productData.docs.map(
+          (eachObjData: QueryDocumentSnapshot<DocumentData>) => (
+            <DisplayProduct
+              {...console.log(eachObjData.data().tags)}
+              key={eachObjData.id}
+              productId={eachObjData.id}
+              productIconUrl={eachObjData.data().productIconUrl as string}
+              // userIconUrl={userIconUrl}
+              // userName={userName}
+              productTitle={eachObjData.data().productTitle as string}
+              productAbstract={eachObjData.data().productAbstract as string}
+              postDate={eachObjData.data().postDate as string}
+              // editDate={eachObjData.data().editDate as string}
+              sumLike={eachObjData.data().sumLike as number}
+            />
+          )
+        )}
+    </SimpleGrid>
+  );
+};
 
 export default FilterProductByTag;
