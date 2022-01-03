9d5962f303c53e5f263017974d6d22207cd79346
yochimonji <yochimonji@gmail.com>
2022-01-03 13:56:33 +0000

    fix typo修正

--- src/components/Home/DisplayTagList.tsx
+++ src/components/Home/DisplayTagList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tag, TagLabel, TagCloseButton, HStack } from "@chakra-ui/react";
 
-const DisplayList = (): JSX.Element => {
+const DisplayTagList = (): JSX.Element => {
   const tagList: string[] = ["React", "TypeScript"];
   return (
     <HStack w="70%" textAlign="center" spacing={4} minW="450px">
@@ -23,4 +23,4 @@ const DisplayList = (): JSX.Element => {
   );
 };
 
-export default DisplayList;
+export default DisplayTagList;
