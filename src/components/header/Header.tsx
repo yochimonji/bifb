275ce1e6524e1249583cbaf1dc9d170611a58798
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-02-10 20:45:58 +0900

    refactor: 実装をより簡潔に変更

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,36 +1,30 @@
 import React from "react";
 import { Box, Container, HStack, Link } from "@chakra-ui/react";
-import { useHistory } from "react-router-dom";
-
+import { Link as ReactLink } from "react-router-dom";
 import { HeaderMenu } from "..";
 
 /**
  * ヘッダーを生成する関数
  * @returns Header Component
  */
-const Header = (): JSX.Element => {
-  const history = useHistory();
-  return (
-    <Box bg="#99CED4" h="16" as="header">
-      <Container maxW="container.lg" h="100%">
-        <HStack justify="space-between" h="100%">
-          <Link
-            href="/"
-            fontSize="4xl"
-            fontWeight="bold"
-            color="gray.100"
-            _hover={{ textDecoration: "none" }}
-            onClick={() => {
-              history.push("/");
-            }}
-          >
-            BiFB
-          </Link>
-          <HeaderMenu />
-        </HStack>
-      </Container>
-    </Box>
-  );
-};
+const Header = (): JSX.Element => (
+  <Box bg="#99CED4" h="16" as="header">
+    <Container maxW="container.lg" h="100%">
+      <HStack justify="space-between" h="100%">
+        <Link
+          as={ReactLink}
+          to="/"
+          fontSize="4xl"
+          fontWeight="bold"
+          color="gray.100"
+          _hover={{ textDecoration: "none" }}
+        >
+          BiFB
+        </Link>
+        <HeaderMenu />
+      </HStack>
+    </Container>
+  </Box>
+);
 
 export default Header;
