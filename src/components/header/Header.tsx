d43c0dd2fb8416697b44a92ae73bfbf2f8d73b54
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-02-10 12:52:22 +0900

    feat: ヘッダーにおける「BiFB」の文字が押されたときの対応
    荒技感しかないが、一応挙動としてはうまくいっている

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Container, HStack, Link } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 import { HeaderMenu } from "..";
 
@@ -7,23 +8,29 @@ import { HeaderMenu } from "..";
  * ヘッダーを生成する関数
  * @returns Header Component
  */
-const Header = (): JSX.Element => (
-  <Box bg="#99CED4" h="16" as="header">
-    <Container maxW="container.lg" h="100%">
-      <HStack justify="space-between" h="100%">
-        <Link
-          href="/"
-          fontSize="4xl"
-          fontWeight="bold"
-          color="gray.100"
-          _hover={{ textDecoration: "none" }}
-        >
-          BiFB
-        </Link>
-        <HeaderMenu />
-      </HStack>
-    </Container>
-  </Box>
-);
+const Header = (): JSX.Element => {
+  const history = useHistory();
+  return (
+    <Box bg="#99CED4" h="16" as="header">
+      <Container maxW="container.lg" h="100%">
+        <HStack justify="space-between" h="100%">
+          <Link
+            href="/"
+            fontSize="4xl"
+            fontWeight="bold"
+            color="gray.100"
+            _hover={{ textDecoration: "none" }}
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            BiFB
+          </Link>
+          <HeaderMenu />
+        </HStack>
+      </Container>
+    </Box>
+  );
+};
 
 export default Header;
