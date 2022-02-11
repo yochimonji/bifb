ff1112ef62cec0dfb73ffbc6a942fd7c7352f2b8
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-02-11 11:33:24 +0900

    refactor: isMounted に関する記述の変更

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -34,7 +34,6 @@ const Home = (): JSX.Element => {
 
   // 作品データの取得
   useEffect(() => {
-    const isMounted = true; // mount状態を監視する変数
     // 即時関数を使って非同期でプロダクトデータを読み込む
     // eslint-disable-next-line no-void
     void (async () => {
@@ -97,9 +96,7 @@ const Home = (): JSX.Element => {
             }
           });
         });
-        if (isMounted) {
-          setProductData(newProductData);
-        }
+        setProductData(newProductData);
       }
     })();
   }, [sortType, searchCondition, searchStatus]);
