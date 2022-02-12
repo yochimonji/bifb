76743520feaae6b5ff370c821379e4af9ccf24dc
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-02-12 12:37:30 +0900

    refactor: 複数形を s から List に変更

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  Input,
-  InputGroup,
-  InputRightElement,
-  VStack,
-  IconButton,
-  SimpleGrid,
-  Button,
-} from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement, VStack, IconButton, SimpleGrid, Button } from "@chakra-ui/react";
 import { BrowserRouter, useHistory } from "react-router-dom";
 import { MdSearch } from "react-icons/md";
 import { QueryDocumentSnapshot, DocumentData } from "firebase/firestore";
@@ -15,47 +7,47 @@ import { fetchAllTags } from "../firebase/firestore";
 
 const Search = (): JSX.Element => {
   const history = useHistory();
-  const [tagscom, setTagsCom] = useState<string[]>();
-  const [tagsList, setTagsList] = useState<string[]>();
-  const tagsAll = [""];
-  let tagsNow = [""];
+  const [tagListcom, setTagListCom] = useState<string[]>();
+  const [tagList, setTagList] = useState<string[]>();
+  const tagListAll = [""];
+  let tagListNow = [""];
   const [newInputText, setNewInputText] = useState<string>("");
 
-  // 最初1回のみ、すべてのタグの取得
+  // ページ読み込み時、1回のみすべてのタグを取得
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const tmpFetchTags = fetchAllTags().then((data) => {
       data.docs.forEach((eachData: QueryDocumentSnapshot<DocumentData>) => {
-        tagsAll.push(eachData.id);
-        tagsNow.push(eachData.id);
+        tagListAll.push(eachData.id);
+        tagListNow.push(eachData.id);
       });
 
-      if (tagsAll[0] === "") {
-        tagsAll.shift();
-        tagsNow.shift();
+      if (tagListAll[0] === "") {
+        tagListAll.shift();
+        tagListNow.shift();
       }
 
-      setTagsCom(tagsAll);
-      setTagsList(tagsAll);
+      setTagListCom(tagListAll);
+      setTagList(tagListAll);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const checkTags = (input: string) => {
-    tagsNow = [""];
-    if (tagscom) {
-      tagscom.forEach((tag: string) => {
+    tagListNow = [""];
+    if (tagListcom) {
+      tagListcom.forEach((tag: string) => {
         const searchText = new RegExp(input, "i");
         const test = tag.search(searchText);
         if (test !== -1) {
-          tagsNow.push(tag);
+          tagListNow.push(tag);
         }
       });
-      if (tagsNow[0] === "") {
-        tagsNow.shift();
+      if (tagListNow[0] === "") {
+        tagListNow.shift();
       }
     }
-    setTagsList(tagsNow);
+    setTagList(tagListNow);
   };
 
   // input欄の入力値の検出
@@ -107,8 +99,8 @@ const Search = (): JSX.Element => {
           </InputRightElement>
         </InputGroup>
         <SimpleGrid columns={[1, 2, 3]} justifyItems="center" w="100%" spacing="40px" pb="40px">
-          {tagsList &&
-            tagsList.map((eachTags) => (
+          {tagList &&
+            tagList.map((eachTags) => (
               <Button
                 key={eachTags}
                 id="button"
