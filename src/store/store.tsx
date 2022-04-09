41d81cacc6099dc26be6f1622302ef6949578b99
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-04-09 17:43:22 +0900

    feat: hooks の作成

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -37,3 +37,6 @@ const reducer = (
 const store = createStore(reducer);
 
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
