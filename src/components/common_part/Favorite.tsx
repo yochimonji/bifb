bf2f8ebaac09b7761eb11bef0d63781ae7f4c4c3
ltoppyl <toppyajibutu@gmail.com>
2022-05-09 15:45:27 +0900

    refactor: 変数名の変更

--- src/components/common_part/Favorite.tsx
+++ src/components/common_part/Favorite.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { HStack, Icon, Text, Box } from "@chakra-ui/react";
 import { AiOutlineHeart } from "react-icons/ai";
 
-type FavoriteNumProps = {
+type Props = {
   favoriteNum: number;
 };
 
-const Favorite = (props: FavoriteNumProps): JSX.Element => (
+const Favorite = (props: Props): JSX.Element => (
   <HStack alignItems="center" spacing={0}>
     <Box w="50%" h="50%" alignItems="center">
       <Icon aria-label="center" as={AiOutlineHeart} />
